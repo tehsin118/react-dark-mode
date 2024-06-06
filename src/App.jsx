d93102b0d8ad5b63bf8e5d78395dc7918c0f3170
Muhammad Tehsin <tehsin118@gmail.com>
2024-06-06 10:53:36 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
-import useLocalStorage from "use-local-storage";
-
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Toggle } from "./components/Toggle";
 
-export const App = () => {
-  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   return (
-    <div className="App" data-theme={isDark ? "dark" : "light"}>
-      <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
-      <h1 className="title">Hello world!</h1>
-      <div className="box">
-        <h2>This is a box</h2>
+    <>
+      <div className="banner  ">
+        <h2>Theme Toggle Example</h2>
+
+        <button onClick={toggleTheme}>
+          {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
+        </button>
       </div>
-    </div>
+    </>
   );
-};
+}
+
+export default App;
